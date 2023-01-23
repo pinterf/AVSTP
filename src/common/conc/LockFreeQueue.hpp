7bd52567ff2de2010a9d583ddcd4985b350709ab
Ferenc Pintér <pinterfer@gmail.com>
2023-01-23 12:09:45 +0100

    Fix freeze

--- src/common/conc/LockFreeQueue.hpp
+++ src/common/conc/LockFreeQueue.hpp
@@ -23,6 +23,7 @@ http://sam.zoy.org/wtfpl/COPYING for more details.
 /*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
 #include <cassert>
+#include <mutex>
 
 
 
@@ -49,6 +50,8 @@ LockFreeQueue <T>::LockFreeQueue ()
 template <class T>
 void	LockFreeQueue <T>::enqueue (CellType &cell)
 {
+	::std::lock_guard<::std::mutex> lock(mx_lockfree_enqueue);
+
 	cell._next_ptr = nullptr;  // set the cell next pointer to NULL
 
 	CellType *     tail_ptr = nullptr;
@@ -58,20 +61,26 @@ void	LockFreeQueue <T>::enqueue (CellType &cell)
 	do	// try until enqueue is done
 	{
 		// read the tail modification count
-		// read the tail cell
+		// read the tail cell (ptr)
 		_m_ptr->_tail.get (tail_ptr, icount);
 
-		// try to link the cell to the tail cell
-		void *         old_ptr = tail_ptr->_next_ptr.cas (&cell, nullptr);
+		CellType* next_ptr = tail_ptr->_next_ptr;
+		const intptr_t icount_tst = _m_ptr->_tail.get_val();
+		if (icount == icount_tst) { // if tail = Tail : ensures that next is still a valid pointer
+			if (next_ptr == nullptr) {
+				// try to link the cell to the tail cell
+				void *         old_ptr = tail_ptr->_next_ptr.cas (&cell, nullptr);
 
-		if (old_ptr == nullptr)
-		{
-			cont_flag = false;	// enqueue is done, exit the loop
-		}
-		else
-		{
-			// tail was not pointing to the last cell, try to set tail to the next cell
-			_m_ptr->_tail.cas2 (tail_ptr->_next_ptr, icount + 1, tail_ptr, icount);
+				if (old_ptr == nullptr)
+				{
+					cont_flag = false;	// enqueue is done, exit the loop
+				}
+			}
+			else
+			{
+				// tail was not pointing to the last cell, try to set tail to the next cell
+				_m_ptr->_tail.cas2 (tail_ptr->_next_ptr, icount + 1, tail_ptr, icount);
+			}
 		}
 	}
 	while (cont_flag);
@@ -79,68 +88,83 @@ void	LockFreeQueue <T>::enqueue (CellType &cell)
 	_m_ptr->_tail.cas2 (&cell, icount + 1, tail_ptr, icount);
 }
 
-
+// static in class
+template <class T>
+::std::mutex LockFreeQueue <T>::mx_lockfree_dequeue;
+template <class T>
+::std::mutex LockFreeQueue <T>::mx_lockfree_enqueue;
 
 // Returns 0 if the queue is empty.
 template <class T>
 typename LockFreeQueue <T>::CellType *	LockFreeQueue <T>::dequeue ()
 {
+	CellType* out_ptr = nullptr;
+
 	constexpr int  max_loop = 100;
 	int            loop_cnt = 0;
 	intptr_t       ocount   = 0;
 	intptr_t       icount   = 0;
 	CellType *     head_ptr = nullptr;
+	CellType *     tail_ptr = nullptr;
 	CellType *     next_ptr = nullptr;
 
-	do	// try until dequeue is done
-	{
-		// read the head modification count
-		// read the head cell
-		_m_ptr->_head.get (head_ptr, ocount);
-		icount   = _m_ptr->_tail.get_val ();   // read the tail modification count
-		next_ptr = head_ptr->_next_ptr;        // read the next cell
+	{ // mutex scope block begin
+		::std::lock_guard<::std::mutex> lock(mx_lockfree_dequeue);
 
-		const intptr_t ocount_tst = _m_ptr->_head.get_val ();
-		if (ocount == ocount_tst)  // ensures that next is a valid pointer to avoid failure when reading next value
+		do	// try until dequeue is done
 		{
-			if (head_ptr == _m_ptr->_tail.get_ptr ())   // is queue empty or tail falling behind ?
+			// read the head modification count
+			// read the head cell
+			_m_ptr->_head.get (head_ptr, ocount);
+			_m_ptr->_tail.get (tail_ptr, icount); // read the tail modification count
+			next_ptr = head_ptr->_next_ptr;        // read the next cell
+
+			const intptr_t ocount_tst = _m_ptr->_head.get_val ();
+			if (ocount == ocount_tst)  // ensures that next is a valid pointer to avoid failure when reading next value
 			{
-				if (next_ptr == nullptr)   // is queue empty ?
+				if (head_ptr == tail_ptr)   // is queue empty or tail falling behind ?
 				{
-					return nullptr; // queue is empty: return NULL
+					if (next_ptr == nullptr)   // is queue empty ?
+					{
+						return nullptr; // queue is empty: return NULL
+					}
+					// tail is pointing to head in a non empty queue, try to set tail to the next cell
+					// newptr newval; compptr compval. If does not match with the sample during the exchange cas2 returns false
+					// Here the return value is ignored
+					_m_ptr->_tail.cas2 (next_ptr, icount + 1, tail_ptr, icount); // tail_ptr is equal to head_ptr
 				}
-				// tail is pointing to head in a non empty queue, try to set tail to the next cell
-				_m_ptr->_tail.cas2 (next_ptr, icount + 1, head_ptr, icount);
+				else if (next_ptr != nullptr) // if we are not competing on the dummy next
+				{
+					// try to set tail to the next cell
+					// newptr newval; compptr compval. If does not match with the sample during the exchange cas2 returns false
+					out_ptr = head_ptr;
+					if (_m_ptr->_head.cas2 (next_ptr, ocount + 1, head_ptr, ocount))
+					{
+						break;   // dequeue done, exit the loop
+					}
+				}
 			}
-			else if (next_ptr != nullptr) // if we are not competing on the dummy next
+
+			++ loop_cnt;
+			if (loop_cnt >= max_loop)
 			{
-				// try to set tail to the next cell
-				if (_m_ptr->_head.cas2 (next_ptr, ocount + 1, head_ptr, ocount))
-				{
-					break;   // dequeue done, exit the loop
-				}
+				// This could indicate that the queue is:
+				// - corrupted
+				// - or in heavy contention
+				assert (false);
+				return nullptr;
 			}
 		}
+		while (true);
+	} // for ending the scope of the mutex
 
-		++ loop_cnt;
-		if (loop_cnt >= max_loop)
-		{
-			// This could indicate that the queue is:
-			// - corrupted
-			// - or in heavy contention
-			assert (false);
-			return nullptr;
-		}
-	}
-	while (true);
-
-	if (head_ptr == &_m_ptr->_dummy)   // check wether we’re trying to pop the dummy cell
+	if (out_ptr == &_m_ptr->_dummy)   // check whether we’re trying to pop the dummy cell
 	{
-		enqueue (*head_ptr);    // this is the dummy cell: push it back to the fifo
-		head_ptr = dequeue ();  // and pop a cell again
+		enqueue(*out_ptr);    // this is the dummy cell: push it back to the fifo
+		out_ptr = dequeue();  // and pop a cell again
 	}
 
-	return head_ptr;
+	return out_ptr;
 }
 
 
